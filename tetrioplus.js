3262bb723ca2e4162ab77877adf7b31990eadcb6
square3ang <sideofsidegd@gmail.com>
2024-09-04 18:31:25 +0900

    edit

--- tetrioplus.js
+++ tetrioplus.js
@@ -1,3 +1,56 @@
+function base64ArrayBuffer(arrayBuffer) {
+    var base64 = ''
+    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+    var bytes = new Uint8Array(arrayBuffer)
+    var byteLength = bytes.byteLength
+    var byteRemainder = byteLength % 3
+    var mainLength = byteLength - byteRemainder
+
+    var a, b, c, d
+    var chunk
+
+    // Main loop deals with bytes in chunks of 3
+    for (var i = 0; i < mainLength; i = i + 3) {
+        // Combine the three bytes into a single integer
+        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+        // Use bitmasks to extract 6-bit segments from the triplet
+        a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+        b = (chunk & 258048) >> 12 // 258048   = (2^6 - 1) << 12
+        c = (chunk & 4032) >> 6 // 4032     = (2^6 - 1) << 6
+        d = chunk & 63               // 63       = 2^6 - 1
+
+        // Convert the raw binary segments to the appropriate ASCII encoding
+        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+    }
+
+    // Deal with the remaining bytes and padding
+    if (byteRemainder == 1) {
+        chunk = bytes[mainLength]
+
+        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+
+        // Set the 4 least significant bits to zero
+        b = (chunk & 3) << 4 // 3   = 2^2 - 1
+
+        base64 += encodings[a] + encodings[b] + '=='
+    } else if (byteRemainder == 2) {
+        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+        a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
+        b = (chunk & 1008) >> 4 // 1008  = (2^6 - 1) << 4
+
+        // Set the 2 least significant bits to zero
+        c = (chunk & 15) << 2 // 15    = 2^4 - 1
+
+        base64 += encodings[a] + encodings[b] + encodings[c] + '='
+    }
+
+    return base64
+}
+
+
 createRewriteFilter("Koreanify", "https://tetr.io/js/tetrio.js*", {
     enabledFor: async (storage, request) => {
         return true;
@@ -51,7 +104,7 @@ createRewriteFilter("Koreanify", "https://tetr.io/js/tetrio.js*", {
               Kt({header:"Enjoyed Duo?",msg:\`The Duo mod is a <b>TETR.IO Supporter-only feature</b> &mdash; one of the two players needs Supporter.<br><b>$\{St(xt("zenith_party_other_username").textContent.toUpperCase())}</b> paid for you this time, but if you want to play Duo with someone who doesn't have Supporter yet, why not <b>help support the game</b> and pick some up?\`,color:"#ff9d00",icon:"/res/icon/friends.svg",classes:["snotify"],timeout:15e3,onclick:e=>{xi(),e()}})
              }`)*/
 
-        //TODO: ADD SRC FETCH
+        src += await (await fetch("https://raw.githubusercontent.com/tetriokoreanpatch/tetrio-korean-patch/main/translate.js")).text();
 
         callback({
             type: 'text/javascript',
@@ -66,10 +119,11 @@ createRewriteFilter("Korean Font Png", "https://tetr.io/res/font/hun.png", {
         return true;
     },
     onStop: async (storage, url, src, callback) => {
-        //TODO: ADD DATA
+        var data = await (await fetch("https://raw.githubusercontent.com/tetriokoreanpatch/tetrio-korean-patch/main/TETRIO_KOFONT_GENERATOR/output/hun.png")).arrayBuffer();
+
         callback({
             type: 'image/png',
-            data: "",
+            data: "data:image/png;base64," + base64ArrayBuffer(data),
             encoding: 'base64-data-url'
         });
     }
@@ -80,10 +134,10 @@ createRewriteFilter("Korean Font Fnt", "https://tetr.io/res/font/hun.fnt", {
         return true;
     },
     onStop: async (storage, url, src, callback) => {
-        //TODO: ADD DATA
+        var data = await (await fetch("https://raw.githubusercontent.com/tetriokoreanpatch/tetrio-korean-patch/main/TETRIO_KOFONT_GENERATOR/output/hun.fnt")).arrayBuffer();
         callback({
             type: 'image/png',
-            data: "",
+            data: "data:image/png;base64," + base64ArrayBuffer(data),
             encoding: 'base64-data-url'
         });
     }
