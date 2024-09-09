03469aa6477c1b4106e8c532d48d9ff9afd93b1c
square3ang <square3ang@gmail.com>
2024-09-09 19:35:49 +0900

    레디 셋 고

--- tetrioplus.js
+++ tetrioplus.js
@@ -80,9 +80,9 @@ createRewriteFilter("Koreanify", "https://tetr.io/js/tetrio.js*", {
             '"LEVEL\\nCOMPLETE"': '"레벨 완료"',
             '{timer:"time",stopwatch:"time",level:"level",lines:"lines",allclears:"all clears",hold:"hold",pieces:"pieces",pieces_duo:"pieces",finesse_l:"finesse",finesse:"finesse",keys:"inputs",score:"score",spp:"score",garbage:"garbage",attack:"attack",attack_duo:"attack",vs:"VS score",kills:"KO\'s",kills_duo:"KO\'s",placement:"placement"};':
                 '{timer:"시간",stopwatch:"시간",level:"레벨",lines:"줄",allclears:"올 클리어",hold:"홀드",pieces:"블록",pieces_duo:"블록",finesse_l:"피네스",finesse:"피네스",keys:"입력",score:"점수",spp:"점수",garbage:"쓰레기 줄",attack:"공격",attack_duo:"공격",vs:"경쟁 점수",kills:"처치",kills_duo:"처치",placement:"놓은 수"};',
-            'create("ready")': 'create("제자리에")',
-            'create("set")': 'create("준비")',
-            'create("GO!")': 'create("시작!")',
+            'create("ready")': 'create("레디")',
+            'create("set")': 'create("셋")',
+            'create("GO!")': 'create(e.stride?"고!":"시작!")',
             'create(t.stats.combo-1+" \\fc3COMBO")': 'create(t.stats.combo-1+" \\fc3콤보")',
             '${t.finesse.faults} fault${1===t.finesse.faults?"":"s"}': '실수 ${t.finesse.faults}개',
             '["","HALL OF BEGINNINGS","THE HOTEL","THE CASINO","THE ARENA","THE MUSEUM","ABANDONED OFFICES","THE LABORATORY","THE CORE","CORRUPTION","PLATFORM OF THE GODS"]':
